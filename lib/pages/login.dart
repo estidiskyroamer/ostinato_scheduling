c3fa17dbc1964bb90a93c21ad7d642239b99e142
Steady Kambodji <maltron.2000@gmail.com>
2025-01-10 11:29:17 +0700

    Add version on login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -9,6 +9,7 @@ import 'package:ostinato/models/user.dart';
 import 'package:ostinato/pages/navigation.dart';
 import 'package:ostinato/pages/register.dart';
 import 'package:ostinato/services/auth_service.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -24,6 +25,9 @@ class _LoginPageState extends State<LoginPage> {
 
   late User? _user;
 
+  PackageInfo info =
+      PackageInfo(appName: '', packageName: '', version: '', buildNumber: '');
+
   void doLogin() async {
     setState(() {
       _isLoading = true;
@@ -34,8 +38,6 @@ class _LoginPageState extends State<LoginPage> {
         .then((value) async {
       if (value) {
         _user = await AuthService().getMe();
-        /* _teacherDetail = await TeacherService().getTeacherDetail();
-        Teacher teacher = _teacherDetail!.data; */
 
         setState(() {
           _isLoading = false;
@@ -59,9 +61,17 @@ class _LoginPageState extends State<LoginPage> {
     });
   }
 
+  void getPackageInfo() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    setState(() {
+      info = packageInfo;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    getPackageInfo();
   }
 
   @override
@@ -70,73 +80,88 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: padding16,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image(
-                  width: MediaQuery.sizeOf(context).width / 2,
-                  image: const AssetImage('assets/images/login.jpeg')),
-              Padding(padding: padding16),
-              Text(
-                "Ostinato",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              Text(
-                "Music Lesson Scheduling",
-                style: Theme.of(context).textTheme.labelMedium,
-              ),
-              Padding(padding: padding16),
-              InputField(
-                textEditingController: emailController,
-                hintText: "Email",
-                inputType: TextInputType.emailAddress,
-                isReadOnly: _isLoading,
-              ),
-              InputField(
-                textEditingController: passwordController,
-                hintText: "Password",
-                isPassword: true,
-                isReadOnly: _isLoading,
-              ),
-              Padding(padding: padding8),
-              _isLoading
-                  ? Center(
-                      child: SizedBox(
-                        width: MediaQuery.of(context).size.width / 6,
-                        child: Config().loadingIndicator,
-                      ),
-                    )
-                  : Column(
-                      children: [
-                        SolidButton(
-                            action: () {
-                              if (emailController.text.isNotEmpty &&
-                                  passwordController.text.isNotEmpty) {
-                                doLogin();
-                              } else {
-                                toastNotification(
-                                    "Email and password cannot be empty");
-                              }
-                            },
-                            text: "Login"),
-                        Text(
-                          "- or -",
-                          style: Theme.of(context).textTheme.bodyMedium!.merge(
-                              const TextStyle(fontStyle: FontStyle.italic)),
+      body: Container(
+        width: double.infinity,
+        padding: padding16,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                      width: MediaQuery.sizeOf(context).width / 2,
+                      image: const AssetImage('assets/images/login.jpeg')),
+                  Padding(padding: padding16),
+                  Text(
+                    "Ostinato",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+                  Text(
+                    "Music Lesson Scheduling",
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                  Padding(padding: padding16),
+                  InputField(
+                    textEditingController: emailController,
+                    hintText: "Email",
+                    inputType: TextInputType.emailAddress,
+                    isReadOnly: _isLoading,
+                  ),
+                  InputField(
+                    textEditingController: passwordController,
+                    hintText: "Password",
+                    isPassword: true,
+                    isReadOnly: _isLoading,
+                  ),
+                  Padding(padding: padding8),
+                  _isLoading
+                      ? Center(
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width / 6,
+                            child: Config().loadingIndicator,
+                          ),
+                        )
+                      : Column(
+                          children: [
+                            SolidButton(
+                                action: () {
+                                  if (emailController.text.isNotEmpty &&
+                                      passwordController.text.isNotEmpty) {
+                                    doLogin();
+                                  } else {
+                                    toastNotification(
+                                        "Email and password cannot be empty");
+                                  }
+                                },
+                                text: "Login"),
+                            Text(
+                              "- or -",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium!
+                                  .merge(const TextStyle(
+                                      fontStyle: FontStyle.italic)),
+                            ),
+                            OutlineButton(
+                                action: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) =>
+                                          const RegisterPage()));
+                                },
+                                text: "Register")
+                          ],
                         ),
-                        OutlineButton(
-                            action: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => const RegisterPage()));
-                            },
-                            text: "Register")
-                      ],
-                    )
-            ],
-          ),
+                ],
+              ),
+            ),
+            Text(
+              "${info.appName} ${info.version}",
+              style: Theme.of(context).textTheme.labelSmall,
+            )
+          ],
         ),
       ),
     );
